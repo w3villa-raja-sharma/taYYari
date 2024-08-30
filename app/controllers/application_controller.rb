b0affd910f3e094f8239d62e0faf5b0020ba7634
raja sharma <raja.sharma@w3villa.com>
2024-08-30 15:19:25 +0530

    initial commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
-class ApplicationController < ActionController::Base
 
-    # def current_user
-    #     binding.pry
-    # end
-end
+
+class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  
+    protected
+  
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:attachment])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:attachment])
+    end
+  end
+  
